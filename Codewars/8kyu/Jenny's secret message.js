d5a2dbf830408dd3e122e67a00b4015c1f0d5071
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2021-12-24 15:06:55 -0500

    refactor

--- Codewars/8kyu/Jenny's secret message.js
+++ Codewars/8kyu/Jenny's secret message.js
@@ -6,9 +6,5 @@
  */
 
 function greet(name) {
-	if (name === 'Johnny') {
-		return 'Hello, my love!';
-	} else {
-		return 'Hello, ' + name + '!';
-	}
+	return 'Hello, ' + (name == 'Johnny' ? 'my love' : name) + '!';
 }
