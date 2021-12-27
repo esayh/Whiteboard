18c4e697ac6bf8a070b21ed25bb6ccfdc7137d19
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2021-12-26 22:45:54 -0500

    add solution

---
+++ Leetcode/Easy/Palindrome Number.js
@@ -0,0 +1,21 @@
+/*
+ Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not. 
+ */
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+	let int = x.toString().split('').reverse().join('');
+	console.log();
+	if (x === Number(int)) {
+		return true;
+	} else {
+		return false;
+	}
+};
