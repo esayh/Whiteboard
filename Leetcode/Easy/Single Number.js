37bb9c16cfc09653c2865d92ff3bc218ea12978a
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-01-17 13:13:21 -0500

    add solution

---
+++ Leetcode/Easy/Single Number.js
@@ -0,0 +1,27 @@
+/*
+  Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+  You must implement a solution with a linear runtime complexity and use only constant extra space.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+	let singleObj = {};
+
+	for (let i = 0; i < nums.length; i++) {
+		if (!singleObj[nums[i]]) {
+			singleObj[nums[i]] = 1;
+		} else {
+			singleObj[nums[i]]++;
+		}
+	}
+
+	for (let key in singleObj) {
+		if (singleObj[key] === 1) {
+			return key;
+		}
+	}
+};
