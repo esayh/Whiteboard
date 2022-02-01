a42030870fbaf2d3381e88da7174adbe7e822ada
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-02-01 15:52:02 -0500

    add solution

---
+++ CodingJS/Warmup-1/notString.js
@@ -0,0 +1,26 @@
+/*
+  Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+
+Examples
+
+  notString('candy') → not candy
+  notString('x') → not x
+  notString('not bad') → not bad
+ */
+
+function notString(str) {
+	let s = str.split(' ');
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] === 'not') {
+			return s.join(' ');
+		}
+		return 'not ' + s.join(' ');
+	}
+}
+
+function notString(str) {
+	if (str.startsWith('not')) {
+		return str;
+	}
+	return 'not ' + str;
+}
