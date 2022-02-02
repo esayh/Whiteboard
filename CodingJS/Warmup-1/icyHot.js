2699c4050b8352e15b63f9a126cac73ff1f2bdc3
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-02-02 13:51:02 -0500

    add solution

--- CodingJS/Warmup-1/icyHot.js
+++ CodingJS/Warmup-1/icyHot.js
@@ -16,3 +16,10 @@ function icyHot(temp1, temp2) {
 	}
 	return false;
 }
+
+function icyHot(temp1, temp2) {
+	if ((temp1 < 0 && temp2 > 100) || (temp2 < 0 && temp1 > 100)) {
+		return true;
+	}
+	return false;
+}
