3653e2c93baf5f90d75b73372762ac7a4d599845
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-02-07 16:35:48 -0500

    add solution

---
+++ CodingJS/Warmup-2/countXX.js
@@ -0,0 +1,21 @@
+/*
+  Count the number of 'xx' in the given string. 
+  We'll say that overlapping is allowed, so 'xxx' contains 2 'xx'.
+
+Examples
+
+  countXX('abcxx') → 1
+  countXX('xxx') → 2
+  countXX('xxxx') → 3
+ */
+
+function countXX(str) {
+	let count = 0;
+
+	for (let i = 0; i < str.length; i++) {
+		if (str.indexOf('xx', i) === i) {
+			count++;
+		}
+	}
+	return count;
+}
