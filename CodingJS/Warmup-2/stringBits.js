2b5575155501bb46f38f154639f87fe13126eb31
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-02-10 12:53:34 -0500

    add solution

---
+++ CodingJS/Warmup-2/stringBits.js
@@ -0,0 +1,22 @@
+/*
+  Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+Examples
+
+  stringBits('Hello') → Hlo
+  stringBits('Hi') → H
+  stringBits('Heeololeo') → Hello
+ */
+
+function stringBits(str) {
+	let s = '';
+
+	for (let i = 0; i < str.length; i += 2) {
+		s += str[i];
+	}
+	return s;
+}
+
+console.log(stringBits('Hello'));
+console.log(stringBits('Hi'));
+console.log(stringBits('Heeololeo'));
