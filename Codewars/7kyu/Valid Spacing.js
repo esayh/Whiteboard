8fdeb200e894bbfffea37315f29231293cd531e7
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-03-03 20:31:35 -0500

    add solution

--- Codewars/7kyu/Valid Spacing.js
+++ Codewars/7kyu/Valid Spacing.js
@@ -2,7 +2,7 @@
   Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. 
   The function should return either True or False.
 
-For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. 
+	For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. 
  */
 
 function validSpacing(s) {
