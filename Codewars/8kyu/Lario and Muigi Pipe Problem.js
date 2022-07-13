65820fbb03f179ce8bbb2c569dd3d67bcb2d0d53
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-07-13 18:16:02 -0400

    added solution

---
+++ Codewars/8kyu/Lario and Muigi Pipe Problem.js
@@ -0,0 +1,28 @@
+/*
+  Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+  The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+  Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+Task
+  Given the a list of numbers, return a fixed list so that the values 
+  increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+Example
+  Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+ */
+
+function pipeFix(numbers) {
+  if (numbers[0] < 1) {
+    return 
+  }
+  let arr = [];
+  let firstVal = numbers[0]
+  let lastVal = numbers[numbers.length - 1]
+  
+  for (let i = firstVal; i <= lastVal; i++) {
+    arr.push(i)
+  }
+  return arr
+  }
