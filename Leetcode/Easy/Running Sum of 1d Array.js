ca3898b3c23645d69c94c4b90fd6b796f20b6af1
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-08-23 22:44:10 -0400

    solution

--- Leetcode/Easy/Running Sum of 1d Array.js
+++ Leetcode/Easy/Running Sum of 1d Array.js
@@ -12,7 +12,6 @@ Return the running sum of nums.
  */
 var runningSum = function (nums) {
 	for (let i = 1; i < nums.length; i++) {
-		console.log(nums[i]);
 		nums[i] += nums[i - 1];
 	}
 	return nums;
